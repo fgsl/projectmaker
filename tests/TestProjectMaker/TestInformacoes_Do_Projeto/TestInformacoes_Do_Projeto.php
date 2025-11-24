69346c7dfaf8ec2cdf21a0e5498078a28af0aeed
JoaoVictorBig156 <joaovirao@gmail.com>
2025-11-24 14:44:20 -0300

    Add files via upload

---
+++ tests/TestProjectMaker/TestInformacoes_Do_Projeto/TestInformacoes_Do_Projeto.php
@@ -0,0 +1,30 @@
+<?php
+namespace TestProjectMaker\TestInformacoes_Do_Projeto;
+use PHPUnit\Framework\TestCase;
+use ProjectMaker\Informacoes_Do_Projeto\Gestor;
+use PHPUnit\Framework\Attributes\CoversClass;
+#[CoversClass(Gestor::class)]
+
+class TestInformacoes_Do_Projeto extends TestCase
+{
+
+    public function testCraicaoDeObjetos()
+    {
+        $gestor = new Gestor();
+        $this->assertInstanceOf(Gestor::class,$gestor);
+        $gestor->colocarNomeDoProjeto = "Criacao do Banco de Dados";
+        $gestor->especificaoesDoProjeto = "Crie um Banco de Dados para controlar a venda dos produtos";
+        $gestor->colocarTipoDoProjeto = "Remoto";
+        $gestor->autorizarAFinalizacaoDoProjeto = TRUE;
+        $this->assertEquals("Criacao do Banco de Dados", $gestor->colocarNomeDoProjeto);
+        $this->assertEquals("Crie um Banco de Dados para controlar a venda dos produtos", $gestor->especificaoesDoProjeto);
+        $this->assertEquals("Remoto", $gestor->colocarTipoDoProjeto);
+        $this->assertTrue($gestor->autorizarAFinalizacaoDoProjeto);
+    }
+
+
+
+
+
+
+}
